1bde32ded1e3a4cd368b81830000d797b1ee6c92
lyj <157653940@qq.com>
2024-01-28 00:24:21 +0800

    add robot

--- Assets/dust_collector.cs
+++ Assets/dust_collector.cs
@@ -12,13 +12,14 @@ public class dust_collector : MonoBehaviour
     private Rigidbody2D rigidbody; // 刚体组件
     private float timer; // 计时器
     private Vector3 randomDirection;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         // 获取刚体组件
         rigidbody = GetComponent<Rigidbody2D>();
         // 初始化计时器
-        timer = changeDirectionInterval;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -35,6 +36,16 @@ public class dust_collector : MonoBehaviour
 
         // 施加力，使用刚体的AddForce方法
         rigidbody.AddForce(randomDirection * moveSpeed);
+
+        if (rigidbody.velocity.x >= 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
     }
 
     // 生成随机移动方向
@@ -43,7 +54,7 @@ public class dust_collector : MonoBehaviour
         // 生成一个随机的二维向量
         Vector2 random2D = UnityEngine.Random.insideUnitCircle.normalized;
         // 转换为三维向量
-        randomDirection = new Vector3(random2D.x, 0f, random2D.y);
+        randomDirection = new Vector3(random2D.x,random2D.y, 0f);
     }
 
 
